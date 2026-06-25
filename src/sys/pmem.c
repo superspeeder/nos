8eb7941add43709ad399c4acc75c96ac4ab7e75a
superspeeder <andy@delusion.org>
2026-06-25 16:06:57 -0700

    fix bsf assembly intrinsic used in sys/pmem.c

--- src/sys/pmem.c
+++ src/sys/pmem.c
@@ -14,13 +14,13 @@ static struct regionmap_entry *_regionmap_page = nullptr;
 
 static inline uint64_t bsf64(uint64_t n) {
     uint64_t out = 0;
-    asm volatile("bsf %0, %1" : "=r"(out) : "r"(n));
+    asm volatile("bsf %0, %%rax" : "=a"(out) : "r"(n));
     return out;
 }
 
 static inline uint32_t bsf32(uint64_t n) {
     uint32_t out = 0;
-    asm volatile("bsf %0, %1" : "=r"(out) : "r"(n));
+    asm volatile("bsf %0, %%eax" : "=a"(out) : "r"(n));
     return out;
 }
 
@@ -44,7 +44,7 @@ static void *_pmem_get_lowmem_page() {
     for (size_t i = 0; i < 5; i++) {
         if (lowmem_bitmap[i]) {
             uint32_t entry = lowmem_bitmap[i];
-            uint32_t out = bsf32(entry);
+            uint32_t out   = bsf32(entry);
             uint64_t addr  = (i << 17) | (out << 12);
             lowmem_bitmap[i] &= ~(1 << out);
             return (void *)addr;
@@ -58,7 +58,7 @@ struct regionmap_entry {
     size_t    size;
     uint64_t *bitmap;
     uint32_t  available_count;  // this value is maintained so that we can easily skip checking full regions.
-    uint32_t  alloc_cursor; // this is the bitmap entry containing the last allocated page.
+    uint32_t  alloc_cursor;     // this is the bitmap entry containing the last allocated page.
 } __attribute__((aligned(32))); // aligned so that we never have entries which are on different cache lines
 
 enum {
@@ -68,42 +68,46 @@ enum {
 };
 
 static uint64_t rmap_rbitmap[RMAP_MAX_ENTRIES / 64] = {0, 0}; // by default, no available bitmaps
-static size_t num_regions = 0;
+static size_t   num_regions                         = 0;
 
-static void* _pmem_alloc_page_from_region(struct regionmap_entry* region) {
+static void *_pmem_alloc_page_from_region(struct regionmap_entry *region) {
     // as long as the presence of pages doesn't unsync, we hopefully won't be wasting time with this.
 
     uint32_t cursor = region->alloc_cursor;
-    for (uint32_t i = cursor ; i < region->size ; i++) {
+    for (uint32_t i = cursor; i < region->size; i++) {
         uint64_t bitmap_entry = region->bitmap[i];
-        if (bitmap_entry == 0) continue;
+        if (bitmap_entry == 0)
+            continue;
         uint64_t bitidx = bsf64(bitmap_entry);
-        uint64_t mask = 1ULL << bitidx;
+        uint64_t mask   = 1ULL << bitidx;
         region->bitmap[i] &= ~mask;
         region->available_count--;
-        return (void*)(region->base_addr + (i * 64 + bitidx) * 0x1000);
+        return (void *)(region->base_addr + (i * 64 + bitidx) * 0x1000);
     }
 
-    for (uint32_t i = 0 ; i < cursor ; i++) {
+    for (uint32_t i = 0; i < cursor; i++) {
         uint64_t bitmap_entry = region->bitmap[i];
-        if (bitmap_entry == 0) continue;
+        if (bitmap_entry == 0)
+            continue;
         uint64_t bitidx = bsf64(bitmap_entry);
-        uint64_t mask = 1ULL << bitidx;
+        uint64_t mask   = 1ULL << bitidx;
         region->bitmap[i] &= ~mask;
         region->available_count--;
-        return (void*)(region->base_addr + (i * 64 + bitidx) * 0x1000);
+        return (void *)(region->base_addr + (i * 64 + bitidx) * 0x1000);
     }
     return nullptr; // no available pages.
 }
 
 static void *_pmem_alloc_page_from_rmap() {
-    if (_regionmap_page == nullptr) return nullptr;
-    for (size_t rbitmap_idx = 0 ; rbitmap_idx < RMAP_MAX_ENTRIES / 64 ; ++rbitmap_idx) {
+    if (_regionmap_page == nullptr)
+        return nullptr;
+    for (size_t rbitmap_idx = 0; rbitmap_idx < RMAP_MAX_ENTRIES / 64; ++rbitmap_idx) {
         uint64_t rbent = rmap_rbitmap[rbitmap_idx];
-        if (rbent == 0) continue;
-        uint64_t regidx = bsf64(rbent) + (rbitmap_idx << 6);
-        struct regionmap_entry* region = &_regionmap_page[regidx];
-        void* page = _pmem_alloc_page_from_region(region);
+        if (rbent == 0)
+            continue;
+        uint64_t                regidx = bsf64(rbent) + (rbitmap_idx << 6);
+        struct regionmap_entry *region = &_regionmap_page[regidx];
+        void                   *page   = _pmem_alloc_page_from_region(region);
         if (region->available_count == 0) {
             rmap_rbitmap[rbitmap_idx] &= ~(1ULL << regidx); // mark that region is unavailable.
         }
@@ -114,10 +118,10 @@ static void *_pmem_alloc_page_from_rmap() {
     return nullptr; // no available pages in any regions.
 }
 
-static struct regionmap_entry* _pmem_find_region_containing(void* ptr) {
+static struct regionmap_entry *_pmem_find_region_containing(void *ptr) {
     const uint64_t addr = (uint64_t)ptr;
-    for (size_t idx = 0 ; idx < num_regions ; ++idx) {
-        struct regionmap_entry* region = &_regionmap_page[idx];
+    for (size_t idx = 0; idx < num_regions; ++idx) {
+        struct regionmap_entry *region = &_regionmap_page[idx];
         if (region->base_addr <= addr && region->base_addr + region->size > addr) {
             return region;
         }
@@ -125,22 +129,23 @@ static struct regionmap_entry* _pmem_find_region_containing(void* ptr) {
     return nullptr;
 }
 
-static void _pmem_free_page_region(struct regionmap_entry* region, void* ptr) {
-    const uint64_t local_addr = (uint64_t)ALIGN_MEMORY_ADDRESS_DOWN((uint64_t)ptr - region->base_addr, PGSZ_L1);
-    const size_t bitmap_idx = local_addr >> 18;
-    const size_t bitmap_bit_idx = (local_addr >> 12) & 0x3f;
-    const uint64_t mask = 1 << bitmap_bit_idx;
-    const uint64_t old_mask = region->bitmap[bitmap_idx];
+static void _pmem_free_page_region(struct regionmap_entry *region, void *ptr) {
+    const uint64_t local_addr     = (uint64_t)ALIGN_MEMORY_ADDRESS_DOWN((uint64_t)ptr - region->base_addr, PGSZ_L1);
+    const size_t   bitmap_idx     = local_addr >> 18;
+    const size_t   bitmap_bit_idx = (local_addr >> 12) & 0x3f;
+    const uint64_t mask           = 1 << bitmap_bit_idx;
+    const uint64_t old_mask       = region->bitmap[bitmap_idx];
     region->bitmap[bitmap_idx] |= mask; // mark as available.
-    if ((old_mask & mask) == 0) ++(region->available_count);
+    if ((old_mask & mask) == 0)
+        ++(region->available_count);
 }
 
-static void _pmem_free_page(void* ptr) {
+static void _pmem_free_page(void *ptr) {
     const uint64_t addr = (uint64_t)ptr;
     if (addr < LOWMEM_END) {
         _pmem_mark_lowmem_page_available(addr);
     } else {
-        struct regionmap_entry* region = _pmem_find_region_containing(ptr);
+        struct regionmap_entry *region = _pmem_find_region_containing(ptr);
         if (region) {
             _pmem_free_page_region(region, ptr);
         }
@@ -178,15 +183,15 @@ void pmem_create_usable_region(uint64_t paddr, uint64_t size) {
         paddr = LOWMEM_END;
     }
 
-    uint64_t aligned_addr = ALIGN_UP(paddr, PGSZ_L1); // round up to nearest page start.
+    uint64_t aligned_addr   = ALIGN_UP(paddr, PGSZ_L1); // round up to nearest page start.
     uint64_t corrected_size = size - (aligned_addr - paddr);
-    uint64_t aligned_size = ALIGN_DOWN(corrected_size, PGSZ_L1); // make sure we don't try to partially
-    for (uint64_t i = 0 ; i < aligned_size ; i += RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000) {
-        uint64_t ad = aligned_addr + i;
-        uint64_t npg = (aligned_size < RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000ULL ? aligned_size : RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000) / 0x1000;
-        uint64_t bms = ALIGN_UP(npg, 0x40) / 0x40;
+    uint64_t aligned_size   = ALIGN_DOWN(corrected_size, PGSZ_L1); // make sure we don't try to partially
+    for (uint64_t i = 0; i < aligned_size; i += RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000) {
+        uint64_t ad    = aligned_addr + i;
+        uint64_t npg   = (aligned_size < RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000ULL ? aligned_size : RMAP_ENTRY_MAX_PAGE_COUNT * 0x1000) / 0x1000;
+        uint64_t bms   = ALIGN_UP(npg, 0x40) / 0x40;
         uint64_t bmpgs = ALIGN_UP(bms, 0x200) / 0x200; // number of pages needed for bitmap.
-        // TODO: write function for allocating multiple contiguous pages both for high and lowmem areas (the only reason we even distinguish between the two is because lowmem being standardized means we can use it's available space to bootstrap space for region bitmaps from the memory map.
-
+        // TODO: write function for allocating multiple contiguous pages both for high and lowmem areas (the only reason we even distinguish between the two is because lowmem being
+        // standardized means we can use it's available space to bootstrap space for region bitmaps from the memory map.
     }
 }
